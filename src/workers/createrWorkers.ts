9b32af7277f572e65c598e1491274f6580562e63
Anastasia <anastaisafedotova@gmail.com>
2021-06-23 11:22:12 +0200

    refactor: update worker

--- src/workers/createrWorkers.ts
+++ src/workers/createrWorkers.ts
@@ -1,10 +1,14 @@
 import { Worker } from 'worker_threads';
 
-export default (workerId: number): Worker => {
-  return new Worker('./src/workers/transfer.js', {
+export default (workerId: number): void => {
+  const worker = new Worker('./src/workers/transfer.js', {
     workerData: {
       path: './worker.ts',
       workerId: workerId
     }
+  });
+
+  worker.on('message', (result) => {
+    console.log(result);
   });
 }
